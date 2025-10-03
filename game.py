c32eae69a17bcb50eefb80aefbbc81400fe6698e
NatalieR2026 <natalie.radin2026@gbwl.org>
2025-10-03 13:43:04 +0000

    10/3

--- game.py
+++ game.py
@@ -1,3 +1,29 @@
+# List of trivia questions
+trivia = [
+            {
+                'question': 'question body',
+                'answer': 'correct',
+                'options': ['option a', 'option b', 'correct']
+            },
+            {
+                'question': '',
+                'answer': '',
+                'options': ['', '', '']
+            },
+            {
+                'question': '',
+                'answer': '',
+                'options': ['', '', '']
+            },
+            {
+                'question': '',
+                'answer': '',
+                'options': ['', '', '']
+            }
+]
+
+
+# GAME LOOP, call your functions below
 def main():
     print("Starting a new Trivia game...")
     user_name = input("Enter your name: ")
