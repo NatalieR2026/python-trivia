79c16ed609e4b24bfbaafd1de56617bb1f47c923
NatalieR2026 <natalie.radin2026@gbwl.org>
2025-10-08 13:41:10 +0000

    10/8

--- game.py
+++ game.py
@@ -23,6 +23,22 @@ trivia = [
 ]
 
 
+# TO DO: Define functions to ask questions & compute score
+def ask_q(questions, answer, options):
+    is_correct = False
+    # Display the question & options
+
+
+    #for question, options in questions.items
+
+    #for questions in trivia:
+    #    print(question )
+    
+    # Take user input
+    # Check if user input matches correct answer
+    return is_correct
+
+
 # GAME LOOP, call your functions below
 def main():
     print("Starting a new Trivia game...")
